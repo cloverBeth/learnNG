fc06abec7b66cdcdae24e300e6ecb62a15e8230c
Beth <2529326035@qq.com>
2016-11-16 16:29:59 +0800

    ajax demo

---
+++ api/jsonp.php
@@ -0,0 +1,8 @@
+<?php
+	 $_arr=array('a'=>1,'b'=>2,'c'=>3);
+	 $_result=json_encode($_arr);
+	 $_callback=$_GET['callback'];
+
+	 echo $_callback."($_result)";
+
+?>
---
+++ api/test.php
@@ -0,0 +1,7 @@
+<?php
+	if($_POST['url']=='testdata'){
+		echo 'ajax 数据服务加载测试from test.php';
+	}else{
+		echo "加载数据失败~~~~~~~";
+	}
+?>
